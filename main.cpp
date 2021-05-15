4af69ccacf6cd59985e83911b6c3954c7acbc9b3
WhiteBlackGoose <wbg@angouri.org>
2021-05-15 09:22:09 +0300

    minor_upd

--- main.cpp
+++ main.cpp
@@ -5,7 +5,8 @@
 int main()
 {
     AngouriMath::Entity expr("x + 2 sin(x) + 2y");
-    std::cout << expr;
-    //expr.ToString();
+    std::cout << expr << '\n';
+    AngouriMath::Entity m("[[1, 0, 70], [0, 1, 0], [0, 0, 1]] ^ -1");
+    std::cout << m.Simplify();
     return 0;
 }
