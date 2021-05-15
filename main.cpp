4b30b6aea6bdc957d667029218f8b5e03f22c39a
WhiteBlackGoose <wbg@angouri.org>
2021-05-15 10:35:20 +0300

    update

--- main.cpp
+++ main.cpp
@@ -1,12 +1,21 @@
 #include <AngouriMath.h>
 #include <iostream>
 
+using namespace AngouriMath;
 
 int main()
 {
-    AngouriMath::Entity expr("x + 2 sin(x) + 2y");
-    std::cout << expr << '\n';
-    AngouriMath::Entity m("[[1, 0, 70], [0, 1, 0], [0, 0, 1]] ^ -1");
-    std::cout << m.Simplify();
+    // derivative
+    Entity expr = "x * y + 2sin(x * y) - y^x";
+    std::cout << expr.Differentiate("x") << "\n";
+
+    // simplify
+    Entity expr1 = "sin(a + 2)^2 + cos(a + 2)^2 + a / (a b)";
+    std::cout << expr1.Simplify() << "\n";
+
+    // matrix
+    Entity expr2 = "[[3, 4], [a, b]] ^ 2";
+    std::cout << expr2.Simplify() << "\n";
+
     return 0;
 }
